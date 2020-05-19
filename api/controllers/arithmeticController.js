15d15eeaa1d01789c6326675d609a53ae53129f5
rtemperini <raizer@gmail.com>
2020-05-19 14:22:12 -0300

    Update arithmeticController.js
    Code working fine but confusing

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -9,9 +9,9 @@ exports.calculate = function(req, res) {
     res.status(400);
     res.json({ error: err.message });
   });
-
+  // prepend + are for casting the type of data
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
